2e55bde98bce4d72030227168944c0b912c111a9
MaverickPaucar <mrpaucar@espe.edu.ec>
2023-08-25 21:18:18 -0500

    Initial Commit

---
+++ src/test/java/com/banquito/core/productsaccounts/test/ProductAccountServiceTest.java
@@ -0,0 +1,20 @@
+package com.banquito.core.productsaccounts.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductAccountServiceTest {
+    
+    @Test
+    void listAllActives() {
+    }
+
+    @Test
+    void obtainById() {
+    }
+
+    @Test
+    void create() {
+    }
+}
